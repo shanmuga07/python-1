9695483f7c28cb32f8a60f89d9f05760d6870cab
shanmuga07 <36722947+shanmuga07@users.noreply.github.com>
2018-03-21 16:14:21 +0530

    Create sort array.py

---
+++ sort array.py
@@ -0,0 +1,8 @@
+number=int(input("enter the length:"))
+list=[]
+for c in range(0,number):
+    newNo=int(input(''))
+    a=list.append(newNo)
+    print("list of values ")
+list.sort()
+print("Sorted list is",list)
